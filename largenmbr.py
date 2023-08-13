402834d94f7269453adca1a3414307e442f63c7d
jabercmt <jabercse2006@gmail.com>
2023-08-13 12:42:43 +0600

    python basic problem

---
+++ largenmbr.py
@@ -0,0 +1,15 @@
+a = int(input("largest nmber: "))
+b = int(input("largest nmber: "))
+if(a > b):
+    print("largest number is:", a)
+else:
+    print("largest number is:", b)
+
+
+#largest
+#num1 = 20
+#num2 = 30
+#if num1 > num2:
+    #print(num1)
+#else
+    #print(num2)
